292824bc22846acff5e2f9dbba5c5671a4f3f936
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-03-10 22:59:21 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,45 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp2
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            int num1, num2, num3;
+        abc obj = new abc();
+        string r = obj.calc();
+        Console.WriteLine(r);
+        Console.ReadKey();
+        ;
+    }
+}
+class abc
+{
 
-            
-            num1 = Convert.ToInt32(Console.ReadLine());
-           
-            num2 = Convert.ToInt32(Console.ReadLine());
-           
-            num3 = Convert.ToInt32(Console.ReadLine());
-
-            if (num1 > num2)
-            {
-                if (num1 > num3)
-                {
-                    Console.Write("The 1st Number is the greatest");
-                }
-                else
-                {
-                    Console.Write("The 3rd Number is the greatest");
-                }
-            }
-            else
-            if (num2 > num3)
-                Console.Write("The 2nd Number is the greatest");
-            else
-                Console.Write("The 3rd Number is the greatest");
-
-
-            Console.ReadKey();
-        }
-    
+    public string calc(int n1,int n2)
+    {
+        string sum = " ";
+        Convert.ToString(sum);
+        Convert.ToInt32(n1 + n2);
+        sum = n1 + n2;
+        return sum; 
+        
+        
 
     }
 }
